7b8271e9e1122cab65dd7a3b80d9f606a3a6d0e3
namdinesh <namdinesh@users.noreply.github.com>
2018-12-27 22:53:46 +1100

    initial commit

---
+++ rest-web-services/src/main/java/com/rest/webservices/example01/HelloWorldBean.java
@@ -0,0 +1,25 @@
+package com.rest.webservices.example01;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return String.format("Hello World Bean [message = %s]", message);
+	}
+
+}
